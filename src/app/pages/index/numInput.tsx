773ac973dcf99f7b8aa471edcd692e1c3e0fa716
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2025-01-13 10:51:46 -0300

    changed input logic between themes

--- src/app/pages/index/numInput.tsx
+++ src/app/pages/index/numInput.tsx
@@ -2,83 +2,64 @@
 
 import {extendVariants, Input} from "@nextui-org/react";
 import './styling/numInput.css';
-
-//TODO DARKINPUT
-//TODO LIGHTINPUT
+import { useTheme } from "next-themes";
 
 export default function NumInput() {
+  const { theme } = useTheme();
+
+  const CustomInput = extendVariants(Input, {
+    variants: {
+      color: {
+        default:{
+          inputWrapper: theme === 'dark' ? darktheme : lighttheme,
+          input:[
+            "inputColor",
+          ],
+        }
+      },
+    },
+    defaultVariants: {
+      color: "default",
+      textSize: "base",
+      removeLabel: false,
+    },
+  });
+
   return (
     <div>
-      <div id="darkInput" className="w-full pt-3 px-5 flex">
-        <DarkInput className="float-start mr-2" label="Insira as idades" type="number" placeholder="Digite as idades separadas por espaço"></DarkInput>
-        <DarkInput className="max-w-40" label="Desconto (%:)" type="number" placeholder="Insira o valor" id="d1"></DarkInput>
-      </div>
-      <div id="lightInput" className="w-full pt-3 px-5 flex">
-        <LightInput className="float-start mr-2" label="Insira as idades" type="number" placeholder="Digite as idades separadas por espaço"></LightInput>
-        <LightInput className="max-w-40" label="Desconto (%:)" type="number" placeholder="Insira o valor" id="d2"></LightInput>
+      <div className="w-full pt-3 px-5 flex">
+        <CustomInput className="float-start mr-2" label="Insira as idades" type="number" placeholder="Digite as idades separadas por espaço" id='c1'></CustomInput>
+        <CustomInput className="max-w-40" label="Desconto (%:)" type="number" placeholder="Insira o valor" id="c2"></CustomInput>
       </div>
     </div>
   );
 }
 
-const DarkInput = extendVariants(Input, {
-  variants: {
-    color: {
-      default:{
-        inputWrapper:[
-          //global
-          "border",
-          "transition-colors",
-          "dark:bg-darkergreen",
+const darktheme = [
+  //global
+  "border",
+  "transition-colors",
+  "dark:bg-darkergreen",
 
-          //outer shell dark mode
-          'dark:border-darkergreen',
-          'dark:data-[hover=true]:border-lightergreen',
-          'dark:data-[hover=true]:bg-darkergreen',
+  //outer shell dark mode
+  'dark:border-darkergreen',
+  'dark:data-[hover=true]:border-lightergreen',
+  'dark:data-[hover=true]:bg-darkergreen',
 
-          'dark:group-data-[focus=true]:border-lightergreen',
-          'dark:group-data-[focus=true]:bg-darkergreen',
-        ],
-        input:[
-          "inputColor",
-        ],
-      }
-    },
-  },
-  defaultVariants: {
-    color: "default",
-    textSize: "base",
-    removeLabel: false,
-  },
-});
+  'dark:group-data-[focus=true]:border-lightergreen',
+  'dark:group-data-[focus=true]:bg-darkergreen',
+];
+const lighttheme = [
+  //global
+  "border",
+  "transition-colors",
+  "bg-lightgreen",
 
-const LightInput = extendVariants(Input, {
-  variants: {
-    color: {
-      default:{
-        inputWrapper:[
-          //global
-          "border",
-          "transition-colors",
-          "bg-lightgreen",
+  //outer shell dark mode
+  'border-lightgreen',
+  'data-[hover=true]:border-maingreen',
+  'data-[hover=true]:bg-lightgreen',
 
-          //outer shell dark mode
-          'border-lightgreen',
-          'data-[hover=true]:border-maingreen',
-          'data-[hover=true]:bg-lightgreen',
-
-          'group-data-[focus=true]:border-maingreen',
-          'group-data-[focus=true]:bg-lightgreen',
-        ],
-        input:[
-          "inputColor",
-        ],
-      }
-    },
-  },
-  defaultVariants: {
-    color: "default",
-    textSize: "base",
-    removeLabel: false,
-  },
-});
+  'group-data-[focus=true]:border-maingreen',
+  'group-data-[focus=true]:bg-lightgreen',
+];
--- src/app/pages/index/numLabels.tsx
+++ src/app/pages/index/numLabels.tsx
@@ -7,28 +7,23 @@ export default function NumLabels(){
   return(
     <div className="w-full pt-1 px-5 flex">
       <div className="totallabel w-full text-end pr-4 text-currentTextColor">TOTAL PESSOAS: 0</div>
-      <div onClick={()=> makeDiscountEditable(document)} className="descontolabel min-w-40 text-end text-currentTextColor" id='dlabel'>ALTERAR VALOR</div>
+      <div onClick={()=> makeDiscountEditable(document)} className="descontolabel min-w-40 text-end text-currentTextColor" id='dlabel'>{thisSession.desconto === 0 ? 'APLICAR' : 'CANCELAR'}</div>
     </div>
   );
 }
 
 function makeDiscountEditable(doc: Document){
-  const d1 = doc.getElementById('d1')!;
-  const d2 = doc.getElementById('d2')!;
-  const dlabel = doc.getElementById('dlabel');
+  const dlabel = doc.getElementById('dlabel')!;
+  const dbutton = doc.getElementById('c2')!;
 
   if(thisSession.desconto === 0){
-    dlabel!.textContent = 'SALVAR VALOR';
-    d1.removeAttribute('disabled'); 
-    d2.removeAttribute('disabled');
 
+    dlabel.innerText = 'CANCELAR';
     thisSession.desconto = 1;
   }
   else{
-    dlabel!.textContent = 'ALTERAR VALOR';
-    d1.setAttribute('disabled', 'true');
-    d2.setAttribute('disabled', 'true');
 
+    dlabel.innerText = 'APLICAR';
     thisSession.desconto = 0;
   }
 }
