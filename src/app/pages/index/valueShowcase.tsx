320a50b970db2e0070ef5bb4a68e59149871039d
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2025-01-16 18:56:29 -0300

    shortcuts added
    shortcuts for text copy were added (ctrl + 1 and ctrl + 2

--- src/app/pages/index/valueShowcase.tsx
+++ src/app/pages/index/valueShowcase.tsx
@@ -4,12 +4,14 @@ import { useTheme } from 'next-themes';
 import './styling/valueShowcase.css'
 import { Files } from 'lucide-react'
 import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function ValueShowcase(){
   const { theme } = useTheme();
   const valueField1Ref = useRef<HTMLDivElement>(null);
   const valueField2Ref = useRef<HTMLDivElement>(null);
+  const [showPopover1, setShowPopover1] = useState(false);
+  const [showPopover2, setShowPopover2] = useState(false);
 
   useEffect(() => {
     const valueField1 = valueField1Ref.current;
@@ -34,7 +36,41 @@ export default function ValueShowcase(){
     }
   }, []);
 
-  function copyValuesText(doc: Document, name: string){ //TODO
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === '1') {
+        // Call the copyValuesText function when Ctrl+C is pressed
+        copyValuesText(document, 'mainfield1');
+        setShowPopover1(true);
+        setTimeout(() => setShowPopover1(false), 1500); // Hide the popover after 2 seconds
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === '2') {
+        // Call the copyValuesText function when Ctrl+C is pressed
+        copyValuesText(document, 'mainfield2');
+        setShowPopover2(true);
+        setTimeout(() => setShowPopover2(false), 1500); // Hide the popover after 2 seconds
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  function copyValuesText(doc: Document, name: string){
     const txt = doc.getElementById(name)!.innerText;
     navigator.clipboard.writeText(txt);
   }
@@ -42,14 +78,17 @@ export default function ValueShowcase(){
   return(
     <div>
       <div className='titlecop'>COPARTICIPAÇÃO</div>
-      <div className='w-full pt-3 px-5 flex'>
+      <div id='customScrollbarDiv' className='w-full pt-3 px-5 flex'>
 
         <div ref={valueField1Ref} className='w-full float-start valuefield'>
           <div id='title_cp'>
             PARCIAL
-            <Popover color='secondary' placement='top'>
+            <Popover color='secondary' placement='top' isOpen={showPopover1}>
               <PopoverTrigger>
-                <Files onClick={()=> copyValuesText(document, 'mainfield1')} className='float-end mt-3 copyanim' color={`${theme === 'light' ? 'var(--light-highlight)' : 'var(--dark-highlight)'}`}></Files>
+                <div className='float-end mt-3'>
+                <Files onClick={()=> copyValuesText(document, 'mainfield1')} className='copyanim' color={`${theme === 'light' ? 'var(--light-highlight)' : 'var(--dark-highlight)'}`}></Files>
+                <div className="shortcutLabel">CTRL + 1</div>
+                </div>
               </PopoverTrigger>
               <PopoverContent>
                 <div className='copiedText'>Parcial copiado!</div>
@@ -79,9 +118,12 @@ export default function ValueShowcase(){
         <div ref={valueField2Ref} className='w-full valuefield'>
           <div id='title_ct'>
             TOTAL
-            <Popover color='secondary' placement='top'>
+            <Popover color='secondary' placement='top' isOpen={showPopover2}>
               <PopoverTrigger>
-                <Files onClick={()=> copyValuesText(document, 'mainfield2')} className='float-end mt-3 copyanim' color={`${theme === 'light' ? 'var(--light-highlight)' : 'var(--dark-highlight)'}`}></Files>
+                <div className='float-end mt-3'>
+                  <Files onClick={()=> copyValuesText(document, 'mainfield2')} className='copyanim' color={`${theme === 'light' ? 'var(--light-highlight)' : 'var(--dark-highlight)'}`}></Files>
+                  <div className="shortcutLabel">CTRL + 2</div>
+                </div>
               </PopoverTrigger>
               <PopoverContent>
                 <div className='copiedText'>Total copiado!</div>
