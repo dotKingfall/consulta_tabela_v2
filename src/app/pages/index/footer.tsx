a097e38fd8e6516de00e4357bd8f79274a9d2678
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2025-01-16 22:35:56 -0300

    footer adapted to mobile
    and build optimized

--- src/app/pages/index/footer.tsx
+++ src/app/pages/index/footer.tsx
@@ -21,27 +21,52 @@ export default function Footer(){
           </div>
         </div>
       </li>
-      <li>
-        <div className='referralsSection'>
-          GOIÂNIA:
-        </div>
-        <div><a target='_blank' href='/pdf/iGOI.pdf'>Individual</a></div>
-        <div><a target='_blank' href='/pdf/eGOI.pdf'>Empresarial</a></div>
-      </li>
-      <li>
-        <div className='referralsSection'>
-          ANÁPOLIS:
-        </div>
-        <div><a target='_blank' href='/pdf/iANA.pdf'>Individual</a></div>
-        <div><a target='_blank' href='/pdf/eANA.pdf'>Empresarial</a></div>
-      </li>
-      <li>
-        <div className='referralsSection'>
-          BRASÍLIA:
-        </div>
-        <div><a target='_blank' href='/pdf/iBRA.pdf'>Individual</a></div>
-        <div><a target='_blank' href='/pdf/eBRA.pdf'>Empresarial</a></div>
-      </li>
+      <div className='referrallAll'>
+        <li>
+          <div className='referralsSection'>
+            GOIÂNIA:
+          </div>
+          <div><a target='_blank' href='/pdf/iGOI.pdf'>Individual</a></div>
+          <div><a target='_blank' href='/pdf/eGOI.pdf'>Empresarial</a></div>
+        </li>
+        <li>
+          <div className='referralsSection'>
+            ANÁPOLIS:
+          </div>
+          <div><a target='_blank' href='/pdf/iANA.pdf'>Individual</a></div>
+          <div><a target='_blank' href='/pdf/eANA.pdf'>Empresarial</a></div>
+        </li>
+        <li>
+          <div className='referralsSection'>
+            BRASÍLIA:
+          </div>
+          <div><a target='_blank' href='/pdf/iBRA.pdf'>Individual</a></div>
+          <div><a target='_blank' href='/pdf/eBRA.pdf'>Empresarial</a></div>
+        </li>
+      </div>
+      <div className='mobileReferralAll'>
+        <li>
+          <div className='mobileReferralsSection'>
+            GO:
+          </div>
+          <div><a target='_blank' href='/pdf/iGOI.pdf'>IND</a></div>
+          <div><a target='_blank' href='/pdf/eGOI.pdf'>EMP</a></div>
+        </li>
+        <li>
+          <div className='mobileReferralsSection'>
+            AN:
+          </div>
+          <div><a target='_blank' href='/pdf/iANA.pdf'>IND</a></div>
+          <div><a target='_blank' href='/pdf/eANA.pdf'>EMP</a></div>
+        </li>
+        <li>
+          <div className='mobileReferralsSection'>
+            BR:
+          </div>
+          <div><a target='_blank' href='/pdf/iBRA.pdf'>IND</a></div>
+          <div><a target='_blank' href='/pdf/eBRA.pdf'>EMP</a></div>
+        </li>
+      </div>
     </footer>
   );
 }
