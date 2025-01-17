e4e229624bbfc2e1d8291dbfb9eb71a10f688564
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2025-01-16 22:43:21 -0300

    added speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import {Providers} from "./providers";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: "Tabelas Jan 2025",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
           <Providers>
             {children}
+            <SpeedInsights />
           </Providers>
       </body>
     </html>
